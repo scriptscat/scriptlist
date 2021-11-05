8ba79931acc45cf694b38b9bc938c5ada0ac35c6
CodFrm <i@xloli.top>
2021-11-05 10:13:52 +0800

    fix: 测试站无法登录的问题

--- internal/http/script.go
+++ internal/http/script.go
@@ -205,7 +205,7 @@ func (s *Script) getStatisticsToken(ctx *gin.Context) string {
 	stk, _ := ctx.Cookie("_statistics")
 	if stk == "" {
 		stk = utils.RandString(32, 2)
-		ctx.SetCookie("_statistics", stk, 3600*24*30, "/", "", true, true)
+		ctx.SetCookie("_statistics", stk, 3600*24*365, "/", "", false, true)
 	}
 	return stk
 }
