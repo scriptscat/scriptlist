718f47e5068ac024a5e68bbfbf1a4d476a119eaf
王一之 <yz@ggnb.top>
2025-08-21 15:09:47 +0800

    remove: 删除高级统计相关的代码

--- migrations/20230228.go
+++ migrations/20230228.go
@@ -1,9 +1,7 @@
 package migrations
 
 import (
-	"github.com/cago-frame/cago/database/db"
 	"github.com/go-gormigrate/gormigrate/v2"
-	"github.com/scriptscat/scriptlist/internal/model/entity/statistics_entity"
 	"gorm.io/gorm"
 )
 
@@ -11,26 +9,10 @@ func T20230228() *gormigrate.Migration {
 	return &gormigrate.Migration{
 		ID: "20230228",
 		Migrate: func(tx *gorm.DB) error {
-
-			if err := db.Use("clickhouse").
-				Set("gorm:table_options",
-					"ENGINE=ReplacingMergeTree() PARTITION BY script_id "+
-						"ORDER BY visitor_id",
-				).
-				AutoMigrate(&statistics_entity.StatisticsVisitor{}); err != nil {
-				return err
-			}
-			return db.Use("clickhouse").
-				Set("gorm:table_options",
-					"ENGINE=ReplacingMergeTree() PARTITION BY script_id "+
-						"ORDER BY session_id",
-				).AutoMigrate(&statistics_entity.StatisticsCollect{})
+			return nil
 		},
 		Rollback: func(tx *gorm.DB) error {
-			if err := db.Use("clickhouse").Migrator().DropTable(&statistics_entity.StatisticsVisitor{}); err != nil {
-				return err
-			}
-			return db.Use("clickhouse").Migrator().DropTable(&statistics_entity.StatisticsCollect{})
+			return nil
 		},
 	}
 }
