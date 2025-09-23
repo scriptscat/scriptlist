c9600e7b20c4a73ef7df723d0867e451d783ba0b
王一之 <yz@ggnb.top>
2025-09-23 10:04:22 +0800

    修复反馈通知模板链接

--- internal/service/notice_svc/template/comment_create.go
+++ internal/service/notice_svc/template/comment_create.go
@@ -21,7 +21,7 @@ const (
     关闭了反馈
   {{- end -}}
   <hr/>
-  <a href="{{.Config.Url}}/script-show-page/{{.Value.ScriptID}}/issue/{{.Value.IssueID}}/comment#comment-{{.Value.CommentID}}">点击查看原文</a><hr/>您可以在<a href="{{.Config.Url}}/users/notify">个人设置页面</a>中取消本邮件的通知,或者取消对该脚本反馈评论的关注
+  <a href="{{.Config.Url}}/script-show-page/{{.Value.ScriptID}}/issue/{{.Value.IssueID}}#comment-{{.Value.CommentID}}">点击查看原文</a><hr/>您可以在<a href="{{.Config.Url}}/users/notify">个人设置页面</a>中取消本邮件的通知,或者取消对该脚本反馈评论的关注
 `
 )
 
