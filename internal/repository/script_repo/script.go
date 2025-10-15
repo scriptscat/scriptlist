5f5254dc2783759e6467736e1cc724dc258c74a4
王一之 <yz@ggnb.top>
2025-10-15 21:53:23 +0800

    切回下载排序

--- internal/repository/script_repo/script.go
+++ internal/repository/script_repo/script.go
@@ -173,7 +173,7 @@ func (u *scriptRepo) Search(ctx context.Context, options *SearchOptions, page ht
 	case "today_download":
 		tabname := db.Default().NamingStrategy.TableName("script_date_statistics")
 		find = find.Joins(fmt.Sprintf("left join %s on %s.script_id=%s.id and %s.date=?", tabname, tabname, scriptTbName, tabname), time.Now().Format("2006-01-02")).
-			Order(tabname + ".update desc,createtime desc")
+			Order(tabname + ".download desc,createtime desc")
 	case "total_download":
 		tabname := db.Default().NamingStrategy.TableName("script_statistics")
 		find = find.Joins(fmt.Sprintf("left join %s on %s.script_id=%s.id", tabname, tabname, scriptTbName)).
