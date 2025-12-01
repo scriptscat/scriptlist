f95f96ae0dbb9dad3f885206f47ee9e0e99c026f
王一之 <yz@ggnb.top>
2025-12-01 20:29:36 +0800

    提高分数权重

--- internal/repository/script_repo/script.go
+++ internal/repository/script_repo/script.go
@@ -254,7 +254,7 @@ func (u *scriptRepo) SearchByEs(ctx context.Context, options *SearchOptions, pag
 	case "today_download":
 		functionSearch["script_score"] = map[string]interface{}{
 			"script": map[string]interface{}{
-				"source": "def todayDownload = doc['today_download'].size() > 0 ? doc['today_download'].value : 0; todayDownload = todayDownload < 0 ? 0 : todayDownload; return _score * 200 + todayDownload;",
+				"source": "def todayDownload = doc['today_download'].size() > 0 ? doc['today_download'].value : 0; todayDownload = todayDownload < 0 ? 0 : todayDownload; return _score * 500 + todayDownload;",
 			},
 		}
 	case "score":
@@ -294,13 +294,13 @@ func (u *scriptRepo) SearchByEs(ctx context.Context, options *SearchOptions, pag
 	case "total_download":
 		functionSearch["script_score"] = map[string]interface{}{
 			"script": map[string]interface{}{
-				"source": "def totalDownload = doc['total_download'].size() > 0 ? doc['total_download'].value : 0; totalDownload = totalDownload < 0 ? 0 : totalDownload; return _score * 200 + totalDownload;",
+				"source": "def totalDownload = doc['total_download'].size() > 0 ? doc['total_download'].value : 0; totalDownload = totalDownload < 0 ? 0 : totalDownload; return _score * 500 + totalDownload;",
 			},
 		}
 	default:
 		functionSearch["script_score"] = map[string]interface{}{
 			"script": map[string]interface{}{
-				"source": "def todayDownload = doc['today_download'].size() > 0 ? doc['today_download'].value : 0; todayDownload = todayDownload < 0 ? 0 : todayDownload; return _score * 200 + todayDownload;",
+				"source": "def todayDownload = doc['today_download'].size() > 0 ? doc['today_download'].value : 0; todayDownload = todayDownload < 0 ? 0 : todayDownload; return _score * 500 + todayDownload;",
 			},
 		}
 
